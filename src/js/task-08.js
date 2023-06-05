9c572ae45d5fcb2c2cea0f625829168fdf046ad1
Timur Naboka <tn3996580@gmail.com>
2023-06-05 22:54:57 +0300

    do task 8,9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.login-form');
+const emailInput = document.getElementsByName('email')[0];
+const passwordInput = document.getElementsByName('password')[0];
+const submitButton = document.getElementsByTagName('button')[0];
+let obj = {};
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+});
+
+submitButton.addEventListener('click', () => {
+  if (emailInput.value === '' || passwordInput.value === '') {
+    alert('All fields must be filled in');
+  } else {
+    obj = {
+      email: emailInput.value,
+      password: passwordInput.value
+    };
+    
+    console.log(obj);
+    form.reset();
+  }
+});
 
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const buttCl = document.querySelector('.change-color');
+buttCl.parentNode.parentElement.addEventListener('click', e => {
+  
+  const setColor = getRandomHexColor();
+  buttCl.parentNode.parentElement.style.backgroundColor = setColor;
+})
+
+
+
+
+
+
